901d650e2a89573564b091a7d297807705e0fa0c
abolanle111 <ojoayomide571@gmail.com>
2022-10-14 01:18:51 +0100

    initial commit

---
+++ Questions.js
@@ -0,0 +1,59 @@
+const questions = [
+    {
+        number:1,
+        question: 'What does HTML stand for?',
+        answer: 'Hyper Text Markup Language',
+        options:[
+            'Home Tool Markup Language',
+            'Hyper Text Markup Language',
+            'Hyperlinks and Text Markup Language',
+            'Hyper Tool Markup Language'
+        ]
+    },
+    {
+        number:2,
+        question: 'What does CSS stand for?',
+        answer: 'Cascading Style Sheets',
+        options:[
+            'Computer Style Sheets',
+            'Creative Style Sheets',
+            'Colorful Style Sheets',
+            'Cascading Style Sheets'
+        ]
+    },
+    {
+        number:3,
+        question: 'What does PHP stand for?',
+        answer: 'Hypertext Preprocessor',
+        options:[
+            'Prepocessed Hypertext Page',
+            'Hypertext Preprocessor',
+            'Processor Hypertext Processor',
+            'None of the above'
+        ]
+    },
+    {
+        number:4,
+        question: 'Which of this is a CSS framework?',
+        answer: 'Bootstrap',
+        options:[
+            'HTML',
+            'Bootstrap',
+            'React',
+            'Vue'
+        ]
+    },
+    {
+        number:5,
+        question: 'Which of these is a type of CSS?',
+        answer: 'All of the above',
+        options:[
+            'Inline CSS',
+            'Embedded CSS',
+            'External CSS',
+            'All of the above'
+        ]
+    }
+]
+
+// You can add more questions like this 
